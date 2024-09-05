25c1eb6f0265187d9ca62abab39c4bcca48554a9
Moazzam Ahmed <moazzamgodil@gmail.com>
2024-09-06 01:31:37 +0500

    Fixed browser minify package

--- lib/types/lib/web3numbers.d.ts
+++ lib/types/lib/web3numbers.d.ts
@@ -22,4 +22,30 @@ export declare const Web3BigNumber: (number: bigint | string | number) => {
     min: (num2: number | string | bigint) => string;
     value: () => string;
 };
+declare global {
+    interface IWeb3BigNumber {
+        toBig(decimals?: number): string;
+        toSmall(decimals?: number): string;
+        toBigInt(): bigint;
+        toCommaSeparated(): string;
+        toCompactNumber(): string;
+        toPercent(): string;
+        toInteger(): number;
+        isZero(): boolean;
+        isNegative(): boolean;
+        equals(num2: number | string | bigint): boolean;
+        gt(num2: number | string | bigint): boolean;
+        gte(num2: number | string | bigint): boolean;
+        lt(num2: number | string | bigint): boolean;
+        lte(num2: number | string | bigint): boolean;
+        add(num2: number | string | bigint): string;
+        subtract(num2: number | string | bigint): string;
+        multiply(num2: number | string | bigint): string;
+        divide(num2: number | string | bigint): string;
+        max(num2: number | string | bigint): string;
+        min(num2: number | string | bigint): string;
+        value(): string;
+    }
+    function Web3BigNumber(number: bigint | string | number): IWeb3BigNumber;
+}
 //# sourceMappingURL=web3numbers.d.ts.map
